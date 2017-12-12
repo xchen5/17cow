1919d34da3d2f75a95dfc22a6e76c31cb702e5bf
Xavier Chen <xchen5@stuy.edu>
2017-12-12 13:56:36 -0500

    Stuff

---
+++ find.js
@@ -0,0 +1,29 @@
+var box = document.getElementById("box");
+var boxHeight = box.offsetHeight;
+var boxWidth = box.offsetWidth;
+
+//hardcode target as center
+//randomize later
+var targetX = boxWidth / 2;
+var targetY = boxHeight / 2;
+
+
+console.log( "box height: " + boxHeight );
+console.log( "box width: " + boxWidth );
+
+//calculate distance between current mouse pos and target
+var distance = function (x0, y0, x1, y1) {
+    return Math.sqrt((x1-x0)**2+(y1-y0)**2);
+};
+
+
+var findIt = function(e) {
+    console.log(e)
+};
+
+/*
+your OTHER FXNS
+
+*/
+box.addEventListener("mousemove", findIt);
+
